44213f218808167e3b293db1afd104b872fc8621
Alexis Wright <misslexiw@gmail.com>
2024-11-12 09:35:45 -0500

    added new and modified files

---
+++ success.js
@@ -0,0 +1,7 @@
+/*the original code provided would not return page to my homepage, it is returning to a list directory of my folders*/
+/*so i looked up another way to link the homepage online and it works now*/
+
+setTimeout(() => {
+  window.location.href =
+    "http://127.0.0.1:5500/block12/Personal_Website_Pt.2/Personal-Website/home.html";
+}, 5000);
